6c005fc2e602771f03fae99d1efc0fc1a149cd5d
netProgrammer <vova@programmer.net>
2015-10-09 17:07:00 +0300

    Volume of cylinder

---
+++ DEV207.1x/HWIntroductionProblem/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let volumeOfCylinder (r: float) (h: float) = Math.PI * Math.Pow(r, 2.0) * h
+
+[<EntryPoint>]    
+let main argv =
+    Console.Write("Please enter radius : ")
+    let r = float (Console.ReadLine())
+    Console.Write("Please enter height : ")
+    let h = float (Console.ReadLine())
+    let volume = volumeOfCylinder r h
+    Console.WriteLine("Volume of cylinder is" + (string volume))
+    Console.ReadKey()
+    0 // return an integer exit code
