2dee0fdf59356195b60ad3fe3014cec2fcebb740
netProgrammer <vova@programmer.net>
2015-10-09 15:48:08 +0300

    HW1 Koan

---
+++ DEV207.1x/HWIntroductionKoan/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv = 
+    let mutable a = 100
+    let b = 1000
+
+    a <- 101
+
+    let f x y = x + y / 10
+
+    Console.WriteLine("Result " + (string (f a b)))
+    Console.ReadKey()
+    0 // return an integer exit code
