363af4927138abf3e5ae64dff8625d6dfe4ce9d0
netProgrammer <vova@programmer.net>
2015-10-13 10:23:59 +0300

    Conditionals And Loops Koan

---
+++ DEV207.1x/HWConditionalsAndLoops/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace HWConditionalsAndLoops.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("HWConditionalsAndLoops")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("HWConditionalsAndLoops")>]
+[<assembly: AssemblyCopyright("Copyright ©  2015")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("f1e74090-2d43-4165-8508-341c9b597728")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ DEV207.1x/HWConditionalsAndLoops/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let rec dosomethingrandom (x : int) =
+  if x = 0 then 1
+  else dosomethingrandom (x - 1) * x
+
+[<EntryPoint>]
+let main argv = 
+   Console.Write("Please enter number : ")
+   let canparse, x = Int32.TryParse(Console.ReadLine())
+   if canparse then Console.WriteLine("Result is " + (string (dosomethingrandom x)))
+   else  Console.WriteLine("The input is wrong")
+   Console.ReadKey()
+   0 // return an integer exit code
