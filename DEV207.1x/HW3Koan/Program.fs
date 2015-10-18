90b60b0f05c8d4a076b858b46d7a0bf7c0971987
netProgrammer <vova@programmer.net>
2015-10-18 16:49:29 +0300

    Math.Sqrt

---
+++ DEV207.1x/HW3Koan/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+let goldenratio = (1.0 + Math.Sqrt(5.0)) / 2.0
+let calculateRatio x = x * goldenratio
+let isEven x = x % 2 = 0
+
+let lista = [for i in 1 .. 10 do
+                 if isEven i then yield calculateRatio (float i)]
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
